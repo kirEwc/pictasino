f3a51a6ef7bd08d2c686fa10522c7e204b930bb1
AirDev <aldairguerracedeno@gmail.com>
2025-02-04 11:55:59 -0500

    cambios en el navbar simples

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,24 +1,24 @@
 'use client'
-import { Navbar as HeroUINavbar } from "@heroui/navbar";
+import { Navbar as HeroUINavbar, NavbarBrand } from "@heroui/navbar";
 import { Button } from "@heroui/react";
 import Image from "next/image";
 
 export const Navbar = () => {
   return (
     <HeroUINavbar maxWidth="full" position="sticky" className="bg-gray-950">
-      <div className="flex flex-row ">
-      {/* <Sidebar /> */}
-      <h1 className="font-serif flex flex-row justify-center items-center pr-3">
-        <Image src={'/images/logo.png'} alt="logo" width={30} height={30} />
-        Pictasino
-      </h1>
-      </div>
+       <NavbarBrand  className="flex flex-row pr-3 gap-2">
+        
+      <Image src={'/images/logo.png'} alt="logo" width={30} height={30} />
+      <Image src={'/images/pictasino.png'} alt="logo" width={100} height={60} />
+       </NavbarBrand>
+
       <div>
-      <Button className=" mr-3 font-serif" variant="bordered" color="secondary">
-          Iniciar seccion
+      <Button className=" mr-3 font-serif" variant="bordered" color="secondary" onBlur={() => { }} onFocus={() => { }}>
+          <h1 className="font-serif sm:block hidden">Iniciar Sección</h1>
+          <h1 className="font-serif sm:hidden block">Inic. Ses.</h1>
         </Button>
         <Button className="bg-purple-500 font-serif">
-          Registrarse
+          <h1 className="font-serif">Registrarse</h1>
         </Button>
       </div>
     </HeroUINavbar>
