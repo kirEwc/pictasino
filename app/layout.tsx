b8d8f52010b27345f4c42d80061383879061d812
AirDev <aldairguerracedeno@gmail.com>
2025-02-06 00:50:35 -0500

    redireccion de la ruta principal y creacion de la primera card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
 
-import { Navbar } from "@/components/navbar";
-import { TabBar } from "@/components/ui/TabBar";
-import { FixedSidebar } from "@/components/ui/FixedSidebar";
+
 
 export const metadata: Metadata = {
   title: {
@@ -25,21 +23,9 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="es">
       <head />
       <body className="dark text-foreground bg-gray-800 font-serif antialiased">
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex flex-row flex-1">
-            <FixedSidebar />
-            <div className="flex-1">
-              <main className="w-full">
+      <main className="w-full">
                 {children}
               </main>
-              <footer className="w-full h-96 bg-gray-900">
-                <h1>Hello Footer</h1>
-              </footer>
-            </div>
-          </div>
-          <TabBar />
-        </div>
       </body>
     </html>
   );
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -8,12 +8,12 @@ export const Navbar = () => {
     <HeroUINavbar maxWidth="full" position="sticky" className="bg-gray-950">
        <NavbarBrand  className="flex flex-row pr-3 gap-2">
         
-      <Image src={'/images/logo.png'} alt="logo" width={30} height={30} />
-      <Image src={'/images/pictasino.png'} alt="logo" width={100} height={60} />
+      <Image src={'/images/logo.png'} alt="logo" width={70} height={50} />
+      <Image src={'/images/pictasino.png'} alt="logo" width={100} height={60} className="hidden md:block"/>
        </NavbarBrand>
 
       <div>
-      <Button className=" mr-3 font-serif" variant="bordered" color="secondary" onBlur={() => { }} onFocus={() => { }}>
+      <Button className=" mr-1 sm:mr-2 md:mr-3 font-serif" variant="bordered" color="secondary">
           <h1 className="font-serif sm:block hidden">Iniciar Sección</h1>
           <h1 className="font-serif sm:hidden block">Inic. Ses.</h1>
         </Button>
