cfe808361eba3e5453a4f36e64a9c8f0f87e65d7
AirDev <aldairguerracedeno@gmail.com>
2025-02-08 01:30:17 -0500

    agrego y pruebo el boton personalizado

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,22 +3,45 @@ import { images } from "@/components/my-components/carousel/ImagePath";
 import { members } from "@/components/my-components/memberList/members";
 import MemberList from "@/components/my-components/memberList/MemberList ";
 import SportsCard from "@/components/SportsCard";
-
-
+import AnimatedButton from "@/components/ui/BotonAnimado/AnimatedButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full px-4">  
-      <section id='carrusel de imagenes'className="w-full" >   
+    <div className="flex flex-col items-center justify-center w-full px-4">
+      <section id="carrusel de imagenes" className="w-full">
         <Carousel images={images} />
       </section>
 
-      <section id='cards de juegos generales' className="">
-          <SportsCard />
+      <section
+        id="cards de juegos generales"
+        className="flex flex-col lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:gap-4 lg:w-full"
+      >
+        <div>
+        <SportsCard />
+        </div>
+
+        <div>
+        <SportsCard manchaColor="bg-blue-700" />
+        </div>
+          
+        <div className="col-start-1 row-start-2">
+        <SportsCard manchaColor="bg-green-700" />
+        </div>
+          
+        <div className="col-start-2 row-start-2">
+        <SportsCard manchaColor="bg-red-700" />
+        </div>
+          
+        <div className="row-span-2 col-start-3 row-start-1">
+        <SportsCard manchaColor="bg-yellow-700" />
+        </div>
+          
+
       </section>
 
       <section id="tabla de miembros " className="w-full">
         <MemberList initialMembers={members} />
+
       </section>
     </div>
   );
---
+++ components/ui/BotonAnimado/AnimatedButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import type { SVGProps } from 'react';
+import './boton.css'
+
+export  function SolarLogin2Broken(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={1.5}><path strokeLinejoin="round" d="M2.001 11.999h14m0 0l-3.5-3m3.5 3l-3.5 3"></path><path d="M9.002 7c.012-2.175.109-3.353.877-4.121C10.758 2 12.172 2 15 2h1c2.829 0 4.243 0 5.122.879C22 3.757 22 5.172 22 8v8c0 2.828 0 4.243-.878 5.121c-.769.769-1.947.865-4.122.877M9.002 17c.012 2.175.109 3.353.877 4.121c.641.642 1.568.815 3.121.862"></path></g></svg>);
+}
+
+
+const AnimatedButton = () => {
+  return (
+    <button className="button" >
+      <div className="dots_border"></div>
+      {/* <SolarLogin2Broken className="sparkle" /> */}
+      <span className="text_button">Registrarse</span>
+    </button>
+  );
+};
+
+export default AnimatedButton;
